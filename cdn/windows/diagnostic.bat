39175c38539de07507d8461b15c29ba4b890c202
Artem V. Navrotskiy <bozaro@yandex.ru>
2018-08-29 22:37:49 +0300

    Generate unique directory for temporary files

--- cdn/windows/diagnostic.bat
+++ cdn/windows/diagnostic.bat
@@ -1,5 +1,11 @@
-set out=%~dp0report_temp
-set rep=%~dp0report
+@echo on
+setlocal EnableExtensions
+
+:uniqLoop
+set "out=%tmp%\joom~%RANDOM%"
+if exist "%out%" goto :uniqLoop
+
+set rep=%out%\report
 set cat=143277b4305cfcb23573b35ba9d26448e71d8eb4_100_100.jpeg
 mkdir "%out%"
 mkdir "%rep%"
@@ -18,6 +24,7 @@ certutil.exe -urlcache -f "http://img-alt.joomcdn.net/%cat%" "%out%\cat_alt_http
 certutil.exe -urlcache -f "http://img-amz.joomcdn.net/%cat%" "%out%\cat_amz_http.jpg" > "%out%\cat_amz_http.out" 2> "%out%\cat_amz_http.err"
 certutil.exe -urlcache -f "https://img-alt.joomcdn.net/%cat%" "%out%\cat_alt_https.jpg" > "%out%\cat_alt_https.out" 2> "%out%\cat_alt_https.err"
 certutil.exe -urlcache -f "https://img-amz.joomcdn.net/%cat%" "%out%\cat_amz_https.jpg" > "%out%\cat_amz_https.out" 2> "%out%\cat_amz_https.err"
-dir /s /b /a-d "%out%\*" > "%out%\files.lst"
+cd "%out%"
+dir /b /a-d > "%out%\files.lst"
 makecab /d "DiskDirectoryTemplate=%rep%" /d "CabinetNameTemplate=report.cab" /f "%out%\files.lst"
 explorer "%rep%"
