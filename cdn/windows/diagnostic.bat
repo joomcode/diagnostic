3fe8ef276fc74082e65e1869afa81fc70e7934af
Artem V. Navrotskiy <bozaro@yandex.ru>
2018-08-29 11:12:21 +0300

    Add diagnostic script for Windows

---
+++ cdn/windows/diagnostic.bat
@@ -0,0 +1,23 @@
+set out=%~dp0report_temp
+set rep=%~dp0report
+set cat=143277b4305cfcb23573b35ba9d26448e71d8eb4_100_100.jpeg
+mkdir "%out%"
+mkdir "%rep%"
+del /q "%out%\*"
+del "%rep%\report.cab"
+ver > "%out%\version.txt"
+ipconfig /all > "%out%\ipconfig.txt"
+nslookup img.joomcdn.net > "%out%\dns.geo.txt"
+nslookup img-alt.joomcdn.net > "%out%\dns.alt.txt"
+nslookup img-amz.joomcdn.net > "%out%\dns.amz.txt"
+tracert img-alt.joomcdn.net > "%out%\trace.alt.txt"
+tracert img-amz.joomcdn.net > "%out%\trace.amz.txt"
+certutil.exe -urlcache -f "http://iam.gcdn.co/info" "%out%\gcore.txt"
+certutil.exe -urlcache -f "https://ifconfig.co/json" "%out%\external_ip.txt"
+certutil.exe -urlcache -f "http://img-alt.joomcdn.net/%cat%" "%out%\cat_alt_http.jpg" > "%out%\cat_alt_http.out" 2> "%out%\cat_alt_http.err"
+certutil.exe -urlcache -f "http://img-amz.joomcdn.net/%cat%" "%out%\cat_amz_http.jpg" > "%out%\cat_amz_http.out" 2> "%out%\cat_amz_http.err"
+certutil.exe -urlcache -f "https://img-alt.joomcdn.net/%cat%" "%out%\cat_alt_https.jpg" > "%out%\cat_alt_https.out" 2> "%out%\cat_alt_https.err"
+certutil.exe -urlcache -f "https://img-amz.joomcdn.net/%cat%" "%out%\cat_amz_https.jpg" > "%out%\cat_amz_https.out" 2> "%out%\cat_amz_https.err"
+dir /s /b /a-d "%out%\*" > "%out%\files.lst"
+makecab /d "DiskDirectoryTemplate=%rep%" /d "CabinetNameTemplate=report.cab" /f "%out%\files.lst"
+explorer "%rep%"
